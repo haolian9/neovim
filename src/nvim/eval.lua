1a67a3247ab56a4464a28c6ddf9a122de8bf4b74
haoliang <haoliang0.1.2@gmail.com>
2024-07-10 11:13:35 +0800

    wip: let getmousepos accepts row, col params

--- src/nvim/eval.lua
+++ src/nvim/eval.lua
@@ -4088,6 +4088,7 @@ M.funcs = {
     signature = 'getmatches([{win}])',
   },
   getmousepos = {
+    args = {0, 2},
     desc = [=[
       Returns a |Dictionary| with the last known position of the
       mouse.  This can be used in a mapping for a mouse click.  The
@@ -4120,8 +4121,8 @@ M.funcs = {
       |v:mouse_col| and |v:mouse_winid| also provide these values.
     ]=],
     name = 'getmousepos',
-    params = {},
-    signature = 'getmousepos()',
+    params = {{'row', 'integer'}, {'col', 'integer'}},
+    signature = 'getmousepos([{row}, {col}])',
     returns = 'vim.fn.getmousepos.ret',
   },
   getpid = {
